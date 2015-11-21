ab93d343a9e57c0c99c751308efd785689d9c5a9
Simon Reuter <Simon.reuter@germanynet.de>
2015-11-22 00:46:31 +0100

    initial commit

---
+++ addons/bftlight/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"ace_common"};
+        author[] = {ECSTRING(common,ACETeam),"BadGuy (simon84)"};
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgEventHandlers.hpp"
---
+++ addons/bftlight/functions/script_component.hpp
@@ -0,0 +1,1 @@
+#include "\z\ace\addons\bftlight\script_component.hpp"
